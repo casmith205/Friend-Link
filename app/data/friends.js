bcd199fe4673ccd7370b9ff3313714e61b89fb09
Carolyn Smith <casmith205@gmail.com>
2018-04-10 15:30:13 -0500

    set dummy data in friends and set up html route

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+// DATA
+var friendArr = [
+  {
+    name: "Ahmed",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3
+    ]
+  }
+];
+
+// Exporting the Array
+module.exports = friendArray;
